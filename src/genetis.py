17ae74f53db1fc7732df238ee6c2c337af3d2905
Tomb01 <bari.tom01@gmail.com>
2024-07-23 00:08:15 +0200

    First crossover iteration

--- src/genetis.py
+++ src/genetis.py
@@ -12,11 +12,8 @@ def crossover(parent1: Structure, parent2: Structure, constrain_n: int) -> Struc
     n2 = innov2.shape[0]
     n = max(n1, n2)
     
-    # Get common nodes
-    common_nodes_idx = np.nonzero(np.in1d(nodes1, nodes2))[0]
-    common_nodes = np.zeros(n, dtype=bool)
-    common_nodes[common_nodes_idx] = True
-    print(common_nodes, common_nodes_idx)
+    nodes = parent1._nodes
+    trusses = parent1._trusses
     
     genome1 = upper_tri_masking(innov1)
     genome2 = upper_tri_masking(innov2)
@@ -30,24 +27,7 @@ def crossover(parent1: Structure, parent2: Structure, constrain_n: int) -> Struc
     common = np.zeros(k, dtype=bool)
     common[common_idx] = True
     
-    dominant = np.copy(genome1)
-    nodes = parent1._nodes
-    trusses = parent1._trusses
+    print(common)
     
-    for i in range(0, k):
-        if common[i]:
-            # Common connection, get state and area random parent trusses adj
-            inn_row = i // (n1-1)
-            inn_col = i % (n1-1) + 1
-            state = random.choice([parent1._trusses[0,inn_row,inn_col], parent2._trusses[0,inn_row,inn_col]])
-            if state == 1:
-                area = random.choice([parent1._trusses[0,inn_row,inn_col], parent2._trusses[0,inn_row,inn_col]])
-                trusses[0,inn]
-            print(state, area)
-    
-    child = Structure(nodes[0:constrain_n], parent1.elastic_modulus)
-    child._nodes = nodes
-    child._trusses = trusses
-    
-    return child
+    return 
             
